ba8b8039372f7dab09e99071596beda97a35a8a9
Sneha Yadav <snehayadavcoding11@gmail.com>
2026-02-02 09:35:17 +0530

    bubble_asc.cpp

---
+++ Sorting/bubble_asc.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v = {43, 546, 432, 5, 2654, 35, 5, 343, 753, 54};
+    int n = v.size();
+
+    cout << "Before Sorting" << endl;
+    for (int ele : v)
+    {
+        cout << ele << " ";
+    }
+    cout << endl;
+
+    // Bubble Sort - Asending Order
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - i; j++)
+        {
+            if (v[j - 1] > v[j])
+            {
+                swap(v[j - 1], v[j]);
+            }
+        }
+    }
+    cout << "After Sorting" << endl;
+    for (int ele : v)
+    {
+        cout << ele << " ";
+    }
+    return 0;
+}
